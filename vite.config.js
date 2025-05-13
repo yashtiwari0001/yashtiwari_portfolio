4c5f3167537d189685172e58d07130d33529fefb
Yash Tiwari <yashtiwari_1@outlook.com>
2025-05-13 16:47:16 +0530

    Resolved merge conflicts and merged changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
   plugins: [react(), svgr()],
-  server: {
-    port: 3000,
-  }
 })
